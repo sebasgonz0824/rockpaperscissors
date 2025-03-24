6f156107644475d06a6de792d7967d0e562bbaba
Sebastian <sebasgonz0824@gmail.com>
2025-03-24 16:44:05 -0400

    ready_to_play

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 import random as rd
 
 def user_function():
-    user_pick=int(input('''Rock. Paper. Scissors. Shoot! Select what you're throwing! 
+    user_pick=int(input('''\nRock. Paper. Scissors. Shoot! Select what you're throwing! 
     1. Rock
     2. Paper
     3. Scissors\n'''))
@@ -13,7 +13,7 @@ def user_function():
         user_hand='Scissors'
     else:
         print("That wasn't one of the options")
-        user_hand='Game Over'
+        return None
     return (user_hand)    
 
 def opponent_function():
@@ -25,18 +25,51 @@ def opponent_function():
     if opponent_pick == 3:
         opponent_hand='Scissors'
     return (opponent_hand)
-x=0
-while x<3:
+
+y=0
+z=0
+while True:
     user_hand_main = user_function()
+    if user_hand_main==None:
+        print("Please try again")
+        break
     opponent_hand_main = opponent_function()
     print('You have selected',user_hand_main)
     print('Your opponent has selected', opponent_hand_main)
-    # print(user_hand_main)
+    if user_hand_main=='Rock' and opponent_hand_main=='Scissors':
+        print('You win! Rock beats Scissors')
+        y=y+1
+        print("You now have",y,"points")
+    if user_hand_main=='Rock' and opponent_hand_main=='Paper':
+        print('You lose! Paper beats Rock')
+        z=z+1
+        print("Your opponent now has ",z,"points")
+    if user_hand_main=='Paper' and opponent_hand_main=='Rock':
+        print('You win! Paper beats Rock')
+        y=y+1
+        print("You now have",y,"points")
+    if user_hand_main=='Paper' and opponent_hand_main=='Scissors':
+        print('You lose! Scissors beats Paper')
+        z=z+1
+        print("Your opponent now has ",z,"points")
+    if user_hand_main=='Scissors' and opponent_hand_main=='Paper':
+        print('You win! Scissors beats Paper')
+        y=y+1
+        print("You now have",y,"points")
+    if user_hand_main=='Scissors' and opponent_hand_main=='Rock':
+        print('You lose! Rock beats Scissors')
+        z=z+1
+        print("Your opponent now has",z,"points")
+
     if user_hand_main==opponent_hand_main:
         print("It's a tie! Play again")
-        x=x+1
-        print("You guys now both have",x,"points")
-    if x==3:
+    if y==3:
+        print("You win! Congratulations!")
         print("Game Over")
         break
-        
+    if z==3:
+        print("You lose! Better luck next time!")
+        print("Game Over")
+        break
+    print("The score is now | You:",y,"Opponent:",z)
+
